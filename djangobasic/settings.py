b777deca9a3d42c29034876a32f836f06bc5ace1
souzadfs <souzadf09@gmail.com>
2024-01-25 21:16:50 -0300

    running

--- djangobasic/settings.py
+++ djangobasic/settings.py
@@ -76,10 +76,11 @@ WSGI_APPLICATION = 'djangobasic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.db.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
